470efa92041dc3af9f50ae158eedd1c6aa90674d
tonchis <lucas.tolchinsky@gmail.com>
2011-11-01 19:34:10 -0300

    Primer draft de RWLock con inanicion.

--- src/backend/RWLock.cpp
+++ src/backend/RWLock.cpp
@@ -1,18 +1,60 @@
 #include "RWLock.h"
 
 RWLock :: RWLock() {
-    pthread_rwlock_init(&(this->rwlock),NULL);
+  sem_init(is_writing_mutex, 0, 1);
+  sem_init(write_complete, 0, 0);
+  is_writing = false;
+
+  sem_init(readers_mutex, 0, 1);
+  sem_init(no_readers, 0, 0);
+  readers = 0;
+}
+
+RWLock :: ~RWLock() {
+  sem_destroy(is_writing_mutex);
 }
 
 void RWLock :: rlock() {
-    pthread_rwlock_rdlock(&(this->rwlock));
+  sem_wait(is_writing_mutex);
+  if(!is_writing){
+    sem_post(is_writing_mutex);
+
+    sem_wait(readers_mutex);
+    readers++;
+    sem_post(readers_mutex);
+  }else{
+    sem_post(is_writing_mutex);
+    sem_wait(write_complete);
+    sem_post(write_complete); // cascada para avisarles a los pibes que se puede leer
+  }
 }
+
 void RWLock :: wlock() {
-    pthread_rwlock_wrlock(&(this->rwlock));
+  sem_wait(readers_mutex);
+  if(readers > 0){
+    sem_post(readers_mutex);
+    sem_wait(no_readers);
+  }
+
+  sem_wait(is_writing_mutex);
+  if(is_writing)
+    sem_wait(write_complete);
+  is_writing = true;
+  sem_post(is_writing_mutex);
 }
+
 void RWLock :: runlock() {
-    pthread_rwlock_unlock(&(this->rwlock));
+  sem_wait(readers_mutex);
+  readers--;
+  if(readers == 0)
+    sem_post(no_readers);
+  sem_post(readers_mutex);
 }
+
 void RWLock :: wunlock() {
-    pthread_rwlock_unlock(&(this->rwlock));
+  sem_wait(is_writing_mutex);
+  is_writing = false;
+  sem_post(is_writing_mutex);
+  sem_post(write_complete);
 }
+
--- src/backend/RWLock.h
+++ src/backend/RWLock.h
@@ -1,17 +1,25 @@
 #ifndef RWLock_h
 #define RWLock_h
 #include <iostream>
+#include <sempahore.h>
 
 class RWLock {
-    public:
-        RWLock();
-        void rlock();
-        void wlock();
-        void runlock();
-        void wunlock();
+  public:
+    RWLock();
+    ~RWLock();
+    void rlock();
+    void wlock();
+    void runlock();
+    void wunlock();
 
-    private:
-        pthread_rwlock_t rwlock;
+  private:
+    bool is_writing;
+    sem_t *is_writing_mutex;
+    sem_t *write_complete;
+
+    int readers;
+    sem_t *readers_mutex;
+    sem_t *no_readers;
 };
 
 #endif
